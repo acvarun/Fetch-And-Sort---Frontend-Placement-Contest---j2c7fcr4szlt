407229028cd0398e5c8c0502baf8b133c472242d
Varun A C <96919265+acvarun@users.noreply.github.com>
2023-03-19 15:52:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,24 +7,48 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [sortAscending, setSortAscending] = useState(true);  
 
+  const handleClick= async ()=>{
+    setIsLoading(true)
+    const res= await fetch("https://content.newtonschool.co/v1/pr/main/users")
+    const data= await res.json()
+    setIsLoading(false)
+    setUsers(data)
+  }
+
+  const handleSorting=()=>{
+    const users1=[...users].sort((a,b)=>{
+      if(sortAscending){
+        return a.name.length-b.name.length
+      }
+      else{
+        return b.name.length-a.name.length
+      }
+    })
+    setUsers(users1)
+    setSortAscending(!sortAscending)
+  }
+
   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn">Fetch User Data</button>
-      <button className="sort-btn">
-        "Sort by name length (ascending)"
-        "Sort by name length (descending)"
+      <button className="fetch-data-btn" onClick={handleClick}>Fetch User Data</button>
+      <button className="sort-btn" onClick={handleSorting}>
+        {sortAscending ?  "Sort by name length (ascending)" :
+        "Sort by name length (descending)"}
       </button>
-      <p>Loading...</p>
+      {isLoading ? <p>Loading...</p> :
       <div className='users-section'>
-          <li>
-            <section className='id-section'></section>
+          {users.map((item)=>
+          <li key={item.id}>
+            <section className='id-section'>{item.id}</section>
             <section className='name-email-section'>
-              <p className='name'>Name: </p>
-              <p className='email'>Email: </p>
+              <p className='name'>Name: {item.name}</p>
+              <p className='email'>Email: {item.email}</p>
             </section>
           </li>
-      </div>
+          )
+        } 
+      </div>}
     </div>
   )
 }
